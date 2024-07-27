91a299007207bb42fb2ae629121349ab3528888b
Memexurer <34003944+Memexurer@users.noreply.github.com>
2024-07-27 17:22:31 +0200

    improve script

--- photopea_script.js
+++ photopea_script.js
@@ -3,11 +3,7 @@ var input = INPUT
 var lays = app.activeDocument.layers;
 
 for (var i = 0; i < lays.length; i++) {
-  if (lays[i].name === "title") {
-    lays[i].textItem.contents = input["title"];
-  }
-
-  if (lays[i].name === "subtitle") {
-    lays[i].textItem.contents = input["subtitle"];
+  if (input[lays[i].name]) {
+    lays[i].textItem.contents = input[lays[i].name];
   }
 }
